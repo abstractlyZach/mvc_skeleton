48dab4e003036225a9919a3775d197734dad3342
Zach Lee <zach.lee@concur.com>
2018-01-18 15:17:13 -0800

    Add controller

---
+++ mvc/controller.py
@@ -0,0 +1,10 @@
+class Controller(object):
+    """Handles input by posting events to the event manager when input
+    happens."""
+    def __init__(self, event_manager, model):
+        self._event_manager = event_manager
+        self._event_manager.register_listener(self)
+        self._model = model
+
+    def notify(self):
+        pass
