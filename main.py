ae962f3ab282e05425b8a32fab9c633578fa3aa6
Zach Lee <zach.lee@concur.com>
2018-05-15 13:24:37 -0700

    Add comments

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ def main():
     set_up_logging()
     # handles initialize, quit, and ticks
     main_event_manager = eventmanager.EventManager()
+    # allows controller to post events to the model
     input_event_manager = eventmanager.EventManager()
+    # create model, view, and controller
     game_model = model.Model(main_event_manager, input_event_manager)
     keyboard = controller.Controller(main_event_manager,
                                      input_event_manager,
                                      game_model)
     graphics = view.GraphicalView(main_event_manager, game_model)
+    # start the game
     game_model.run()
 
 
