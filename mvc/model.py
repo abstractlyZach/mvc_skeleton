7dd254c0b06933bb6612a62396ab390caf3fc602
Zach Lee <zach.lee@concur.com>
2018-05-15 11:36:53 -0700

    Move text handling to the model
    Why was the view handling that???

--- mvc/model.py
+++ mvc/model.py
@@ -11,10 +11,18 @@ class Model(object):
         self._event_manager.register_listener(self)
         self._running = False
         self._clock = pygame.time.Clock()
+        self._text = 'The View is drawing on your screen!'
+        self._starting_new_word = False
+
+    @property
+    def text(self):
+        return self._text
 
     def notify(self, event):
         if isinstance(event, events.QuitEvent):
             self._running = False
+        elif isinstance(event, events.KeyPressEvent):
+            self._handle_keypress(event)
 
     def run(self):
         """Starts the game loop. Pumps a tick into the event manager for
@@ -25,3 +33,31 @@ class Model(object):
             tick = events.TickEvent()
             self._event_manager.post(tick)
             self._clock.tick(settings.FPS)
+
+    def _handle_keypress(self, event):
+        key_text = pygame.key.name(event.key)
+        if len(key_text) == 1:
+            self._update_text_with_char_input(key_text)
+        else:
+            self._handle_non_alphanum_keypress(key_text)
+
+    def _update_text_with_char_input(self, new_char):
+        if self._starting_new_word:
+            self._text = new_char
+            self._starting_new_word = False
+        else:
+            self._text += new_char
+
+    def _handle_non_alphanum_keypress(self, key_text):
+        if key_text == 'return':
+            self._starting_new_word = True
+        elif key_text == 'space':
+            self._update_text_with_char_input(' ')
+        elif key_text == 'backspace':
+            self._backspace()
+        else:
+            pass
+
+    def _backspace(self):
+        self._text = self._text[:-1]
+
--- mvc/view.py
+++ mvc/view.py
@@ -1,3 +1,4 @@
+import logging
 import pygame
 
 from mvc import events
@@ -11,8 +12,6 @@ class GraphicalView(object):
         self._is_initialized = False
         self._screen = None
         self._small_font = None
-        self._text = 'The View is drawing on your screen!'
-        self._starting_new_word = False
 
     def notify(self, event):
         if isinstance(event, events.QuitEvent):
@@ -21,40 +20,11 @@ class GraphicalView(object):
             self.initialize()
         elif isinstance(event, events.TickEvent):
             self.render_all()
-        elif isinstance(event, events.KeyPressEvent):
-            self._handle_keypress(event)
 
     def _handle_quit(self):
         self._is_initialized = False
         # ends the pygame graphical display
         pygame.quit()
-
-    def _handle_keypress(self, event):
-        key_text = pygame.key.name(event.key)
-        if len(key_text) == 1:
-            self._update_text_with_char_input(key_text)
-        else:
-            self._handle_non_alphanum_keypress(key_text)
-
-    def _update_text_with_char_input(self, new_char):
-        if self._starting_new_word:
-            self._text = new_char
-            self._starting_new_word = False
-        else:
-            self._text += new_char
-
-    def _handle_non_alphanum_keypress(self, key_text):
-        if key_text == 'return':
-            self._starting_new_word = True
-        elif key_text == 'space':
-            self._update_text_with_char_input(' ')
-        elif key_text == 'backspace':
-            self._backspace()
-        else:
-            pass
-
-    def _backspace(self):
-        self._text = self._text[:-1]
 
 
     def render_all(self):
@@ -62,7 +32,11 @@ class GraphicalView(object):
             return
         # clear display
         self._screen.fill((0, 0, 0))
-        some_words = self._small_font.render(self._text, True, (0, 255, 0))
+        some_words = self._small_font.render(
+            self._model.text,
+            True,
+            (0, 255, 0)
+        )
         self._screen.blit(some_words, (0, 0))
         pygame.display.update()
 
@@ -74,4 +48,4 @@ class GraphicalView(object):
         self._screen = pygame.display.set_mode((600, 60))
         self._small_font = pygame.font.Font(None, 40)
         self._is_initialized = True
-        print('VIEW INITIALIZED')
+        logging.info('VIEW INITIALIZED')
